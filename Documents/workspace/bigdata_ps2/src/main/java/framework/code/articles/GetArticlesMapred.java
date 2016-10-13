4d8baf49305b5ae90bda548cdeb663c4f4994862
Brando Rondon <brando@brandeis.edu>
2016-10-13 17:44:51 -0400

    ~~~ small insignificant changes across committed files: 	* pre-pended "framework." to all 	package imports
    ~~~ bigger changes:
    	* wrote set-up method and job
    	config in main for GetArticlesMapRed
    	
    	
    ~~~ fixes:
    	* all instances of WikipediaPage in 
    	WikipediaPageInputFormat changed to
    	EnglishWikipedia since WikiepediaPage
    	is the inherited abstract class.

--- Documents/workspace/bigdata_ps2/src/main/java/framework/code/articles/GetArticlesMapred.java
+++ Documents/workspace/bigdata_ps2/src/main/java/framework/code/articles/GetArticlesMapred.java
@@ -1,14 +1,26 @@
 package framework.code.articles;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
+
 
 /**
  * This class is used for Section A of assignment 1. You are supposed to
@@ -37,17 +49,53 @@ public class GetArticlesMapred {
 			// TODO: You should implement people articles load from
 			// DistributedCache here
 			super.setup(context);
+			Configuration conf = context.getConfiguration();
+		    Path[] localFiles = DistributedCache.getLocalCacheFiles(conf);
+		    BufferedReader reader = null;
+
+		    for(Path path: localFiles){
+		    	if (path.toString().endsWith("people.txt")){
+		    		reader = new BufferedReader(new FileReader(path.toString()));
+		    		break;
+		    	}
+		    }
+		    String name;
+		    while((name = reader.readLine()) != null){
+		    	peopleArticlesTitles.add(name);
+		    }
+		    reader.close();
+			
 		}
 
 		@Override
 		public void map(LongWritable offset, WikipediaPage inputPage, Context context)
 				throws IOException, InterruptedException {
 			// TODO: You should implement getting article mapper here
+			
+			//if WikipediaPage is an article 
+			//	if page title is contained in the poeple.txt hashmap
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException {
 		// TODO: you should implement the Job Configuration and Job call
 		// here
+		Configuration conf = new Configuration();
+		//assuming we have people.txt at the hdfs root 
+		DistributedCache.addCacheFile(new URI("/people.txt"), conf);
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length != 2){
+			System.err.println("Usage: <get articles jar> <in> <out>");		
+		}
+		Job job = new Job(conf, "Get Articles");
+		job.setJarByClass(GetArticlesMapred.class);
+		job.setMapperClass(GetArticlesMapper.class); 
+		//no shuffling, combining or any sort of reducing occurs
+		job.setNumReduceTasks(0);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true)? 0: 1);	
 	}
 }
